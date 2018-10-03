c795fe1827980385351dcda5c476361e93c71e89
vagrant <vagrant@homestead>
2018-10-03 09:19:43 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="https://weibo.com/wangmingdetiantang/home?wvr=5" target="_blank">Silencetalk</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="mailto:ncuhomer@163.com">联系我们</a></p>
+    </div>
+</footer>
