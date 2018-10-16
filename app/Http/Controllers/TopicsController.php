58e42257e9b7e69d6e03439d83e8ec4333183092
vagrant <vagrant@homestead>
2018-10-16 15:09:38 +0000

    Add Queue for translate slugs

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
+use Auth;
 use App\Handlers\ImageUploadHandler;
+use App\Models\User;
+use App\Models\Link;
+
 class TopicsController extends Controller
 {
     public function __construct()
@@ -15,11 +19,14 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request,Topic $topic)
-	{
-		$topics = $topic->withOrder($request->order)->paginate(30);
-		return view('topics.index', compact('topics'));
-	}
+    public function index(Request $request, Topic $topic, User $user, Link $link)
+    {
+        $topics = $topic->withOrder($request->order)->paginate(20);
+        $active_users = $user->getActiveUsers();
+        $links = $link->getAllCached();
+
+        return view('topics.index', compact('topics', 'active_users', 'links'));
+    }
 
     public function show(Request $request, Topic $topic)
     {
@@ -31,42 +38,43 @@ class TopicsController extends Controller
         return view('topics.show', compact('topic'));
     }
 
-	public function create(Topic $topic)
-	{
-	    $categories = Category::all();
-		return view('topics.create_and_edit', compact('topic'));
-	}
+    public function create(Topic $topic)
+    {
+        $categories = Category::all();
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function store(TopicRequest $request,Topic $topic)
-	{
-		$topic->fill($request->all());
-		$topic->user_id = Auth::id();
-		$topic->save();
-		return redirect()->to($topic->link())->with('message', '成功创建主题.');
-	}
+    public function store(TopicRequest $request, Topic $topic)
+    {
+        $topic->fill($request->all());
+        $topic->user_id = Auth::id();
+        $topic->save();
 
-	public function edit(Topic $topic)
-	{
+        return redirect()->to($topic->link())->with('success', '成功创建主题！');
+    }
+
+    public function edit(Topic $topic)
+    {
         $this->authorize('update', $topic);
         $categories = Category::all();
-		return view('topics.create_and_edit', compact('topic','categories'));
-	}
+        return view('topics.create_and_edit', compact('topic', 'categories'));
+    }
 
-	public function update(TopicRequest $request, Topic $topic)
-	{
-		$this->authorize('update', $topic);
-		$topic->update($request->all());
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+        $topic->update($request->all());
 
-		return redirect()->to($topic->link())->with('message', '更新成功.');
-	}
+        return redirect()->to($topic->link())->with('success', '更新成功！');
+    }
 
-	public function destroy(Topic $topic)
-	{
-		$this->authorize('destroy', $topic);
-		$topic->delete();
+    public function destroy(Topic $topic)
+    {
+        $this->authorize('destroy', $topic);
+        $topic->delete();
 
-		return redirect()->route('topics.index')->with('success', '成功删除.');
-	}
+        return redirect()->route('topics.index')->with('success', '成功删除！');
+    }
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
     {
