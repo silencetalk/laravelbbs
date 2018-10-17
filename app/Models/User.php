7dbef6b76c3f17cd2d95d468f07bdeae38b966f2
vagrant <vagrant@homestead>
2018-10-17 15:01:52 +0000

    List user replies

--- app/Models/User.php
+++ app/Models/User.php
@@ -37,4 +37,8 @@ class User extends Authenticatable
         return $this->id == $model->user_id;
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
