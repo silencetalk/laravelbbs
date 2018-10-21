19f1360f8101a98a10a6f26478816753681126f2
vagrant <vagrant@homestead>
2018-10-21 07:31:59 +0000

    Setup default home pages

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/','PagesController@index')->name('root');
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
